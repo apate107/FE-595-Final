12dd6685c499d8290fe67ad9da471f51fbe2c14a
apate107 <apate107@stevens.edu>
2019-12-05 16:10:58 -0500

    Implement K-NN

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
-from flask import Flask,jsonify,render_template, request, redirect,url_for,flash
+from flask import Flask, jsonify, render_template, request, redirect, url_for, flash
 import os
 from functions import *
+from models import *
 
 global uploadedDataframe
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'MYFAVORITECLASSISFE595'
-app.config['FILE_UPLOADS'] = 'inputfiles'
+app.config['FILE_UPLOADS'] = os.getcwd() + '\\inputfiles'
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 
 ### Home Page
@@ -27,7 +28,6 @@ def uploadfile():
         if 'datafile' not in request.files:
             flash('No file')
             return redirect(request.url)
-        
 
         dfile = request.files["datafile"]
         filename = os.path.join(app.config["FILE_UPLOADS"], dfile.filename)
@@ -37,20 +37,26 @@ def uploadfile():
 
         uploadedDataframe = processInitialFile(filename,dheader,dsep )
         dfData = uploadedDataframe[:5]
-        return render_template("uploadprocessed.html", tables=[dfData.to_html(classes='data')], titles=dfData.columns.values)
+        knn_results = KNN(uploadedDataframe)
+        return render_template("uploadprocessed.html", tables=[dfData.to_html(classes='data'),
+                                                               knn_results.to_html(classes='data')],
+                               titles=dfData.columns.values)
         
     else:
         return render_template("upload.html")
 
+
 @app.errorhandler(404)
 def page_not_found(e):
     return "<h1>404</h1><p>The resource could not be found.</p>", 404
+
 
 @app.after_request
 def add_header(response):
     response.cache_control.max_age = 0
     response.no_cache = True
     return response
+
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0", port=5000)
